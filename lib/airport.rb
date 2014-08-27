45cb76c57c745904e0e8f70981152ac8b3779cbc
Spike Nox <spike01@gmail.com>
2014-08-27 17:06:23 +0100

    Finally got everything working

--- lib/airport.rb
+++ lib/airport.rb
@@ -15,6 +15,7 @@ class Airport
   end
 
   def land(plane)
+    raise "This plane has already landed!" if plane.flight_status == :landed
     return "Airport full. Please try again later" if hangar.count >= capacity 
     return "Bad weather. Please try again later" if weather == :stormy
     hangar << plane
@@ -22,19 +23,26 @@ class Airport
   end
 
   def take_off(plane)
+    raise "This plane is already in the air!" if plane.flight_status == :flying
     return "Bad weather. Please try again later" if weather == :stormy
     hangar.delete(plane)
     plane.flight_status= :flying
   end
 
-  def land_all_planes(sky)
-    sky.delete_if do |plane|
-      true 
-      land(plane) 
+# Methods below are helper methods for final test
+
+  def land_all_planes(planes)
+    return if planes.empty?
+    planes.each do |plane| 
+      land(plane)    
+      planes.delete(plane) if plane.flight_status == :landed
     end
+    land_all_planes(planes)
   end
 
   def fly_all_planes
+    return if hangar.empty?
     hangar.each { |plane| take_off(plane) }
+    fly_all_planes
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,27 +5,24 @@ require 'weather'
 # A plane currently in the airport can be requested to take off.
 #
 # No more planes can be added to the airport, if it's full.
-# It is up to you how many planes can land in the airport and how that is impermented.
+# It is up to you how many planes can land in the airport and how that is implemented.
 #
 # If the airport is full then no planes can land
+
 describe Airport do
 
-  #include Weather
-
   let(:airport) { Airport.new }
-  let(:plane) { double :plane }
+  let(:plane) { Plane.new }
 
   context 'taking off and landing' do
     it 'a plane can land' do
       allow(airport).to receive(:weather).and_return(:sunny)
-      allow(plane).to receive(:flight_status=)
       airport.land(plane)
       expect(airport.hangar.count).to eq(1)
     end
     
     it 'a plane can take off' do
       allow(airport).to receive(:weather).and_return(:sunny)
-      allow(plane).to receive(:flight_status=)
       airport.land(plane)
       airport.take_off(plane)
       expect(airport.hangar.count).to eq(0)
@@ -43,7 +40,7 @@ describe Airport do
     end
 
     it 'can be initialized with a custom capacity' do
-      expect((Airport.new(capacity: 6)).capacity).to eq(6)
+      expect((Airport.new(capacity: 20)).capacity).to eq(20)
     end
 
   context 'traffic control' do
@@ -53,6 +50,7 @@ describe Airport do
       expect(airport.land(plane)).to eq("Airport full. Please try again later")
     end 
   end
+
     # Include a weather condition using a module.
     # The weather must be random and only have two states "sunny" or "stormy".
     # Try and take off a plane, but if the weather is stormy, the plane can not take off and must remain in the airport.
@@ -63,6 +61,8 @@ describe Airport do
     
     context 'weather conditions' do
       it 'a plane cannot take off when there is a storm brewing' do
+        allow(airport).to receive(:weather).and_return(:sunny)
+        airport.land(plane)
         allow(airport).to receive(:weather).and_return(:stormy)
         expect(airport.take_off(plane)).to eq("Bad weather. Please try again later")
       end
@@ -107,6 +107,16 @@ describe Plane do
     airport.take_off(plane)
     expect(plane.flight_status).to eq(:flying)
   end
+  
+  it 'cannot land if it\'s already landed' do
+    allow(airport).to receive(:weather).and_return(:sunny)
+    airport.land(plane)
+    expect{ airport.land(plane) }.to raise_error(RuntimeError)
+  end
+
+  it 'cannot take_off if it\'s already flying' do
+    expect{ airport.take_off(plane) }.to raise_error(RuntimeError)
+  end
 end
 
 # grand finale
@@ -139,23 +149,24 @@ describe "The grand finale (last spec)" do
     expect(airport.hangar.count).to eq(0)
   end
 
-  it 'the airport can land all planes in the sky' do
+  it 'the airport can (eventually) land all planes in any weather' do
     make_planes
     airport.land_all_planes(sky)
     expect(airport.hangar.count).to eq(6)
   end
                                                                      
-                                                                     #def land_planes
-    #until red_arrows.empty? do
-      #red_arrows.each { |plane| airport.land(plane) }
-    #end
-  #end
-
-  xit 'all planes can land and all planes can take off' do
+  it 'the airport can (eventually) fly all its planes in any weather' do
     make_planes
-    land_planes
+    airport.land_all_planes(sky)
+    airport.fly_all_planes
+    expect(airport.hangar.count).to eq(0)
+  end
+
+  it 'all planes can land and all planes can take off' do
+    make_planes
+    airport.land_all_planes(sky)
     expect(airport.hangar.map { |plane| plane.flight_status }).to eq([:landed, :landed, :landed, :landed, :landed, :landed])
-    fly_planes
-    expect(red_arrows.map { |plane| plane.flight_status }).to eq([:flying, :flying, :flying, :flying, :flying, :flying])
+    airport.fly_all_planes
+    expect(airport.hangar.count).to eq(0) # I don't know where my planes are! ...but I'm pretty sure they're flying
   end
 end
